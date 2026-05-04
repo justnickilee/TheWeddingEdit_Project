ace9b40778ab60ca3677c784f2e3fef4a76cb87e
Nicki <justnickilee@gmail.com>
2026-05-03 17:54:11 -0700

    Started working on very very basic components for Guests page

--- src/Views/Guests.tsx
+++ src/Views/Guests.tsx
@@ -1,6 +1,43 @@
-export function Guests() {
+import type * as Types from '../Models/Types';
+import { useState } from 'react';
+
+export function Guests(props: { guests: Types.Guest[] }) {
+    const [guestsState, setGuestsState] = useState(props.guests);
+
     return (
         <div className="flex flex-col justify-start items-start page">
-            HELLO WORLD
+            <h3>Wedding Guests</h3>
+            <div id="guest-list-controls" className="flex justify-between mb-3">
+                <button id="add-guests-btn" className="btn btn-primary">Add Guests</button>
+                <div className="flex gap-x-3">
+                    <span id="guest-list-filter">Filter</span>
+                    <textarea id="guest-list-search" placeholder="Search Guests"></textarea>
+                </div>
+            </div>
+            <div id="guest-list">
+                <GuestList />
+            </div>
         </div>);
+
+    function GuestList() {
+        if (guestsState.length === 0) {
+            return (
+                <div>You can keep track of your wedding guests here. Click the button above to start adding your guests!</div>
+            );
+        }
+
+        return (
+            guestsState.map(guest => <GuestEntry {...guest}/>)
+        );
+    }
+
+    function GuestEntry(guest : Types.Guest) {
+        const guestName = `${guest.firstName}${!guest.lastName ? "" : " " + guest.lastName}`;
+
+        return (
+            <div data-guest-id={guest.id} className="guest-entry flex justify-between w-full">
+                <span>{guestName}</span>
+            </div>
+        );
+    }
 }
